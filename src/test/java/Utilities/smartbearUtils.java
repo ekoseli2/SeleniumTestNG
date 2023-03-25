3c4ae9d56c3a2f11e64d65950a2df5c512dca9ea
Ekrem Koseli <ekoseli2@illinois.edu>
2023-03-24 19:30:11 -0500

    Cleaned

--- src/test/java/Utilities/smartbearUtils.java
+++ src/test/java/Utilities/smartbearUtils.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class smartbearUtils {
-    public void loginForSmartbearUtil(WebDriver driver) {
+    public static void loginForSmartbearUtil(WebDriver driver) {
         WebElement userName = driver.findElement(By.id("ctl00_MainContent_username"));
         WebElement passWord = driver.findElement(By.id("ctl00_MainContent_password"));
         WebElement logIn = driver.findElement(By.id("ctl00_MainContent_login_button"));
