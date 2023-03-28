5524ea334ccc154f15fae5d883afc7dfb437467b
Ekrem Koseli <ekoseli2@illinois.edu>
2023-03-27 20:50:55 -0500

    Cleaned

--- src/test/java/Lesson08/c4_NegativeTest.java
+++ src/test/java/Lesson08/c4_NegativeTest.java
@@ -1,4 +1,48 @@
 package Lesson08;
 
+import Utilities.WebDriverUtil;
+import Utilities.smartbearUtils;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class c4_NegativeTest {
+    // navigate "http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx"
+    // run a series of tests that are designed to fail logging in
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverUtil.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+    }
+
+    @AfterMethod
+    public void cleanUp() {
+        // driver.quit();
+    }
+    @Test (dataProvider = "myLibrary")
+    public void negativeLoginTest(String userID, String passID) {
+        smartbearUtils.negativeLoginForSmartbearUtil(driver, userID, passID);
+    }
+
+    @DataProvider (name = "myLibrary")
+    public static Object[][] invalidUserCredentials() {
+        return new Object[][] {
+                {"ekrem", "renas123"},
+                {"bean", "eat.123"},
+                {"banana","dsfew!@#@"},
+                {"Grandou", "BANodufa"},
+                {"219838182", "<>i2313"},
+                // this one will login properly
+                {"Tester", "test"}
+        };
+    }
+
 }
--- src/test/java/Utilities/smartbearUtils.java
+++ src/test/java/Utilities/smartbearUtils.java
@@ -14,4 +14,14 @@ public class smartbearUtils {
         passWord.sendKeys("test");
         logIn.click();
     }
+
+    public static void negativeLoginForSmartbearUtil(WebDriver driver, String userID, String passID) {
+        WebElement userName = driver.findElement(By.id("ctl00_MainContent_username"));
+        WebElement passWord = driver.findElement(By.id("ctl00_MainContent_password"));
+        WebElement logIn = driver.findElement(By.id("ctl00_MainContent_login_button"));
+
+        userName.sendKeys(userID);
+        passWord.sendKeys(passID);
+        logIn.click();
+    }
 }
