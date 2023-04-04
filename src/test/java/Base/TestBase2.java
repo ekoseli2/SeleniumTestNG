f342df5e608839d6a2a5ff305caa242ed43a9e49
Ekrem Koseli <ekoseli2@illinois.edu>
2023-04-03 19:25:34 -0500

    Cleaned

---
+++ src/test/java/Base/TestBase2.java
@@ -0,0 +1,23 @@
+package Base;
+
+import Utilities.DriverUtil;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
+
+import java.time.Duration;
+
+public abstract class TestBase2 {
+    // because this class is abstract, you cannot use it by itself. It must be subclassed.
+    @BeforeMethod
+    public void setUp() {
+        DriverUtil.getDriver().manage().window().maximize();
+        DriverUtil.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+
+    @Ignore
+    @AfterMethod
+    public void cleanUp() {
+        DriverUtil.closeDriver();
+    }
+}
